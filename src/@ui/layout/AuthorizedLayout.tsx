4e0413385ddbae8e40a1fbe2e8f841d4d76e3d0d
kamilbartusik <kamil.bartusik@napoleoncat.com>
2022-04-22 20:51:51 +0200

    Add basic AuthorizedLayout

---
+++ src/@ui/layout/AuthorizedLayout.tsx
@@ -0,0 +1,24 @@
+import React, {FC, ReactElement} from 'react';
+import TopBar from "./TopBar";
+import SideBar from "./SideBar";
+import MainContent from "./MainContent";
+
+interface IAuthorizedLayoutProps {
+    children: ReactElement
+}
+
+const AuthorizedLayout: FC<IAuthorizedLayoutProps> = ({
+    children
+}: IAuthorizedLayoutProps) => {
+    return (
+        <div>
+            <TopBar />
+            <SideBar />
+            <MainContent>
+                {children}
+            </MainContent>
+        </div>
+    );
+};
+
+export default AuthorizedLayout;
